1792980f93267a2985c930ecd325b93abcd6a844
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-02-01 21:29:25 +0100

    test: add further tests from sample

--- src/WolverineLearnings.Tests/MiddlewareTests.cs
+++ src/WolverineLearnings.Tests/MiddlewareTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Alba;
 using Marten;
@@ -10,6 +11,51 @@ namespace WolverineLearnings.Tests;
 
 public class Test
 {
+  [Fact]
+  public async Task the_application_assembly_is_inferred_correctly()
+  {
+    #region sample_disabling_the_transports_from_web_application_factory
+
+    // This is using Alba to bootstrap a Wolverine application
+    // for integration tests, but it's using WebApplicationFactory
+    // to do the actual bootstrapping
+    await using var host = await AlbaHost.For<Program>(x =>
+    {
+      // I'm overriding 
+      x.ConfigureServices(services => services.DisableAllExternalWolverineTransports());
+    });
+
+    #endregion
+        
+        
+    var options = host.Services.GetRequiredService<WolverineOptions>();
+    options.ApplicationAssembly.ShouldBe(typeof(Account).Assembly);
+  }
+  
+  [Fact]
+  public async Task validation_miss()
+  {
+    await using var host = await AlbaHost.For<Program>();
+
+    var account = new Account
+    {
+      Balance = 1000
+    };
+
+    var store = host.Services.GetRequiredService<IDocumentStore>();
+    await using var session = store.LightweightSession();
+    session.Store(account);
+    await session.SaveChangesAsync();
+
+    var bus = host.Services.GetRequiredService<IMessageBus>();
+
+    await Should.ThrowAsync<Exception>(async () =>
+    {
+      await bus.InvokeAsync(new DebitAccount(account.Id, 0));
+    });
+  }
+
+
   [Fact]
   public async Task hit()
   {
--- src/WolverineLearnings/AssemblyVisitiliby.cs
+++ src/WolverineLearnings/AssemblyVisitiliby.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("WolverineLearnings.Tests")]
+[assembly: InternalsVisibleTo("Learnings.Tests")]
